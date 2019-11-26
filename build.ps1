0805741ee169ce919c0e15b4b64194b1a6b096d8
Sam Goldmann <samuel.goldmann@ancorasoftware.com>
2019-11-25 21:52:52 -0500

    fix image name in build.ps1

--- build.ps1
+++ build.ps1
@@ -2,5 +2,5 @@ Import-Module "$PSScriptRoot\helpers.psm1"
 
 $BuildProps = (Get-GetBuildProperties);
 $Tags = (Get-TagNames);
-$TagArgs = [string]::Join(" ", ($Tags | ForEach-Object{ "-t '$_'"}));
+$TagArgs = [string]::Join(" ", ($Tags | ForEach-Object{ "-t '$($BuildProps.repository)/$_'"}));
 Invoke-Expression -Command "docker build --isolation=hyperv --build-arg WINDOWS_VERSION=$($BuildProps.windowsVersion) --build-arg DOCKER_VERSION=$($BuildProps.dockerVersion) $TagArgs .";
